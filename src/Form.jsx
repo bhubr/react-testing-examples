08ac3a4e24cf68f70fd357f0037ca04d6b18a3e5
Benoît Hubert <benoithubert@gmail.com>
2022-02-22 19:45:15 +0100

    Add simple test to check email field label's presence

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,41 +1,61 @@
+import { useState } from 'react';
 import "./Form.css"
 
+const initialFormState = {
+  email: '',
+  password: '',
+  pictureUrl: '',
+  country: '',
+  diet: '',
+  rememberMe: false,
+}
+
 export default function Form({ onSubmit }) {
+  const [data, setData] = useState(initialFormState)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    onSubmit(data)
+    setData({ ...initialFormState })
+  }
+
+  const handleChange = (key) => ({ target }) => setData(prev => ({ ...prev, [key]: target.value }))
   return (
-    <form className="Form" onSubmit={onSubmit}>
+    <form className="Form" onSubmit={handleSubmit}>
 
       <div className="Form-control">
-        <label htmlFor="email">
-          Email
-          <input id="email" type="email" />
+        <label htmlFor="email" required>
+          Email (*)
+          <input id="email" type="email" value={data.email} onChange={handleChange('email')} />
         </label>
       </div>
 
       <div className="Form-control">
-        <label htmlFor="password">
-          Password
-          <input id="password" type="password" />
+        <label htmlFor="password" required>
+          Password (*)
+          <input id="password" type="password" value={data.password} onChange={handleChange('password')} />
         </label>
       </div>
 
       <div className="Form-control">
         <label htmlFor="pictureUrl">
           Profile picture URL
-          <input id="pictureUrl" type="url" />
+          <input id="pictureUrl" type="url" value={data.pictureUrl} onChange={handleChange('pictureUrl')} />
         </label>
       </div>
 
       <div className="Form-control">
         <label htmlFor="bio">
           Short biography
-          <textarea id="bio" rows="4" />
+          <textarea id="bio" rows="4" value={data.bio} onChange={handleChange('bio')} />
         </label>
       </div>
 
       <div className="Form-control">
         <label htmlFor="country">
-          Country
-          <select id="country">
+          Country (*)
+          <select id="country" value={data.country} onChange={handleChange('country')} required>
+            <option value="">&mdash;</option>
             <option value="FR">France</option>
             <option value="DE">Germany</option>
             <option value="UK">United Kingdom</option>
